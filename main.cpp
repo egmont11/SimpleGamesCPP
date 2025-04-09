85954db5912448ed70c2c6aeb45290b3b2c25b98
Matěj Kubíček <matejkubicek53@gmail.com>
2025-04-09 19:12:45 +0200

    Writing out the board state in the console

--- main.cpp
+++ main.cpp
@@ -27,6 +27,26 @@ int RockPaperScissors() {
 
 int TicTacToe() {
     std::cout << "Very well, let us play tic tac toe." << std::endl;
+
+    char board[9] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
+    bool gameFinished = false;
+    char currentPlayer = 'X';
+
+    while (!gameFinished) {
+        std::string continueRound;
+        std::cout << "Current player: " << currentPlayer << std::endl;
+
+        std::cout << "    1    2    3" << std::endl;
+        std::cout << "A " << "| " << board[0] << " |" << "| " << board[1] << " |"<< "| " << board[2] << " |" << std::endl;
+        std::cout << "B " << "| " << board[3] << " |" << "| " << board[4] << " |"<< "| " << board[5] << " |" << std::endl;
+        std::cout << "C " << "| " << board[6] << " |" << "| " << board[7] << " |"<< "| " << board[8] << " |" << std::endl;
+
+        if (currentPlayer == 'X') currentPlayer = 'O';
+        else if (currentPlayer == 'O') currentPlayer = 'X';
+
+        std::cin >> continueRound;
+    }
+
     return 0;
 }
 
